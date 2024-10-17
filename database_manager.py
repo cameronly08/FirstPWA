394ce0a5feb36886152a2c69da84164fc09590b5
Cameron L <124547283+cameronly08@users.noreply.github.com>
2024-10-17 22:23:05 +0000

    finished first part

--- database_manager.py
+++ database_manager.py
@@ -1,10 +1,8 @@
-
-
 import sqlite3 as sql
 
 def listExtension():
-    con = sql.connect("databaseFiles/database.db")
-    cur = con.cursor()
-    data = cur.execute('SELECT * FROM extension').fetchall()
-    con.close()
-    return data
+  con = sql.connect(".database/data_source.db")
+  cur = con.cursor()
+  data = cur.execute('SELECT * FROM extension').fetchall()
+  con.close()
+  return data
