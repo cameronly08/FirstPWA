ab78952df56cc23899513a7ec6acbfd5eeafd02b
Cameron L <124547283+cameronly08@users.noreply.github.com>
2024-10-20 08:31:29 +0000

    redirect works

--- main.py
+++ main.py
@@ -11,6 +11,20 @@ def index():
    data = dbHandler.listExtension()
    return render_template('/index.html', content=data)
 
+@app.route('/add.html', methods=['POST', 'GET'])
+def add():
+    if request.method == 'POST':
+        email = request.form['email']
+        name = request.form['name']
+        if dbHandler.insertContact(email, name):
+            return render_template('add.html', is_done=True)  # Successful submission
+        else:
+            return render_template('add.html', is_done=False, error="Email already exists.")  # Handle duplicate email
+    else:
+        return render_template('add.html', is_done=False)  # Initial load
+
+
+
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=5000)
 
